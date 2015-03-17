9ce67fafdf91b383abad8aad448376d46bc47d26
Kyle <kylezhou2002@hotmail.com>
2015-03-17 17:08:26 -0400

    ONLY THE BEST PROGRAMZ EVAS

---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim input, greeting As String
+        greeting = "Hello my name is "
+        input = TextBox1.Text
+
+        MessageBox.Show(greeting + input)
+    End Sub
+End Class
